2c3f831c4ae8b59f084129e59cc253e579e61ce5
John Tuttle <jtuttle@conjur.net>
2018-01-18 15:33:37 -0500

    confirmation before posting

--- lib/commands/task_breakdown.rb
+++ lib/commands/task_breakdown.rb
@@ -3,12 +3,14 @@ module WorkflowTools
     class TaskBreakdown
       def self.execute(issue_number, issue_tracking)
         issue = issue_tracking.issue(issue_number)
-
+        
         task_steps = []
         task_step = nil
 
+        say("Task breakdown for #{issue.title}")
+        
         while task_step != ''
-          task_step = ask("Next step?")
+          task_step = ask("What's the next step?")
           task_steps << task_step unless task_step.empty?
         end
 
@@ -17,9 +19,15 @@ module WorkflowTools
         task_steps.each do |step|
           message << "- #{step}\n"
         end
+
+        say("#{message}\n")
+        
+        agree("Post the above task breakdown to Slack?")        
         
         notifier = Slack::Notifier.new(Config.user.slack.webhook_url)
         notifier.ping(message)
+
+        say("Task breakdown was posted.")
       end
     end
   end
